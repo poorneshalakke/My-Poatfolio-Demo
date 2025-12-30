f952ccd0fbb4c80339ab113d87f6336c7cabfb19
poornesh <poornesh160@gmail.com>
2025-12-31 01:16:53 +0530

    builf issue

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a2123c4c06eaee90"; }
+/******/ 		__webpack_require__.h = function() { return "6808f61c9dfc59ea"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
